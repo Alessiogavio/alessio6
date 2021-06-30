6fb6460b5f5da953d3a3b0de18fdbe74d9fc3efe
Alessio <alegavio01@gmail.com>
2021-06-30 13:02:35 +0200

    json

--- TUTORIAL JSON/ES1/my_script.js
+++ TUTORIAL JSON/ES1/my_script.js
@@ -1,10 +1,27 @@
 $("button").click (function(){
 
     $.getJSON( "http://local.voome.tv/hmp-finder/alessio.json",function(obj) {
-        console.log(obj);
-        $.each(obj,function(key,value){
-            $("ul").append("<li>"+value.name+"</li>");
+        console.log(obj);    
+        
+        //let parsed = JSON.parse(utente);
+        //console.log(parsed.clients.client);
+        $.each(obj.clients,function(key,value)
+        {            
+            console.log(obj.clients[0].client);
+            //let parsed = JSON.parse(utente);
+            //console.log (parsed);
+           // $("ul").append("<li>"+value.name+"</li>");
         });
-
+        //document.writeln(parsed.clients);
+    const clients 
+    for(let i=0; i<clients.lenght;i++)
+    {
+        console.log(clients[i])
+    }
     });
+    /*
+    <script>
+        
+    </script>*/
+    
 });
---
+++ TUTORIAL JSON/tut2/my_script.js
@@ -0,0 +1,9 @@
+$("button").click (function(){
+
+    $.getJSON( "http://local.voome.tv/hmp-finder/alessio.json",function(obj) {
+        console.log(obj);
+        $.each(obj,function(key,value){
+            $("ul").append("<li>"+value.name+"</li>");
+        });
+    });
+});
