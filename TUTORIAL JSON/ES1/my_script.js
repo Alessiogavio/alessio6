e9f53f9f7c9ef4d8b0896c2788d6cd45421fe0ff
Alessio <alegavio01@gmail.com>
2021-06-30 17:48:36 +0200

    json

--- TUTORIAL JSON/ES1/my_script.js
+++ TUTORIAL JSON/ES1/my_script.js
@@ -1,27 +1,57 @@
-$("button").click (function(){
+$("button").click (function()
+{
 
-    $.getJSON( "http://local.voome.tv/hmp-finder/alessio.json",function(obj) {
-        console.log(obj);    
-        
-        //let parsed = JSON.parse(utente);
-        //console.log(parsed.clients.client);
-        $.each(obj.clients,function(key,value)
-        {            
-            console.log(obj.clients[0].client);
-            //let parsed = JSON.parse(utente);
-            //console.log (parsed);
-           // $("ul").append("<li>"+value.name+"</li>");
-        });
-        //document.writeln(parsed.clients);
-    const clients 
-    for(let i=0; i<clients.lenght;i++)
+    $.getJSON( "http://local.voome.tv/hmp-finder/alessio.json",function(obj) 
     {
-        console.log(clients[i])
-    }
+        console.log(obj);            
+
+
+            //STAMPA UN SOLO VALORE
+            $.each(obj.clients,function(index, client)
+            {        
+                    
+                console.info(client.client);
+                $.each(client.devices,function(index, device)  
+                {
+                    console.info('  '+device.mac);
+                });
+            });
+
+            /*
+            var $ul=$("<ul></ul>");
+            
+            //CICLO FOR STAMPA TUTTI I CLIENTS
+            obj.clients.forEach(function(obj) 
+            {
+                
+                $ul.append("<li>"+obj.clients+"</li>");
+                    
+                console.log(obj.client)                          
+            });
+               
+            */
+            
+            
+            
+            
+            
+            // $("ul").append("<li>"+value.name+"</li>");            
+
+            //document.writeln(parsed.clients);
+             //
+
+             /*
+                var lista =obj.clients.querySelectorAll("li");
+                for(){
+                    lista[i].addEventListener("mouseover",function(){
+                        this.classList.add("selezione");
+                    });
+                    lista[i].addEventListener("mouseover",function(){
+                        this.classList.add("selezione");
+                    });
+                    lista[i].addEventListener("click",function(){
+                        this.classList.toggle("fatto");
+                    });
+                }*/
     });
-    /*
-    <script>
-        
-    </script>*/
-    
 });
