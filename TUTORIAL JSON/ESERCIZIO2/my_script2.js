e0395624cbf98518bb2a7e187bc9866d4bb6d54e
Alessio <alegavio01@gmail.com>
2021-07-02 18:02:17 +0200

    json

--- TUTORIAL JSON/ESERCIZIO2/my_script2.js
+++ TUTORIAL JSON/ESERCIZIO2/my_script2.js
@@ -5,32 +5,32 @@ $("button").click (function()
     {
         console.log(obj);  
 
-        $.each(obj.clients,function(index,client)
-        {
-            
-            $.each(client.devices,function(index,devices)
-            {
 
-                console.info('  -'+devices.mac);
-                console.info('  -'+devices.type);
-                console.info('  -'+devices.name);
-            });
-            console.info(client.client);
-        });
-        console.log ("  ");
-        console.log("-------------------------");
         console.log ("  ");
 
-        console.log("  mac            type    name          client");
+        console.log("mac            type    name          client");
         $.each(obj.clients,function(index,client)
         {
             
             $.each(client.devices,function(index,devices)
             {               
-                console.info('  -'+devices.mac, ' -'+devices.type, ' -'+devices.name, ' -'+client.client);
+                console.info('-'+devices.mac, ' -'+devices.type, ' -'+devices.name, ' -'+client.client);
             });
         });
 
+        console.log ("  ");
+        console.log("-------------------------");
+        console.log ("  ");
+
+        console.log("type    mac            name          client");
+        $.each(obj.clients,function(index,client)
+        {
+            
+            $.each(client.devices,function(index,devices)
+            {               
+                console.info('-'+devices.type, ' -'+devices.mac, ' -'+devices.name, ' -'+client.client);
+            });
+        });
 
 
     });
